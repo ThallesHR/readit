753c10c6811ff937356eed838bb3003db8adf33f
ThallesHR <thalles.rosa@sou.unifal-mg.edu.br>
2026-03-11 15:22:18 -0300

    crossorigin para o frontend

--- src/main/java/com/th/readit/controller/LinksController.java
+++ src/main/java/com/th/readit/controller/LinksController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("conteudos")
 public class LinksController {
